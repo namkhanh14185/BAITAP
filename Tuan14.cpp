a4d80d82b612bd772ecf2f0d8d8c1a0820223ba5
namkhanh14185 <rbb6820@gmail.com>
2026-06-09 22:25:36 +0700

    fx

--- Tuan14.cpp
+++ Tuan14.cpp
@@ -24,31 +24,6 @@ int getHeight(Node* n) {
     return n->height;
 }
 
-int getBalance(Node* n) {
-    if (n == nullptr) return 0;
-    return getHeight(n->left) - getHeight(n->right);
-}
-
-Node* rightRotate(Node* y) {
-    Node* x = y->left;
-    Node* T2 = x->right;
-    x->right = y;
-    y->left = T2;
-    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
-    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
-    return x;
-}
-
-Node* leftRotate(Node* x) {
-    Node* y = x->right;
-    Node* T2 = y->left;
-    y->left = x;
-    x->right = T2;
-    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
-    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
-    return y;
-}
-
 Node* insert(Node* node, int key) {
     if (node == nullptr) return newNode(key);
     if (key < node->key)
@@ -59,20 +34,6 @@ Node* insert(Node* node, int key) {
         return node;
 
     node->height = 1 + max(getHeight(node->left), getHeight(node->right));
-    int balance = getBalance(node);
-
-    if (balance > 1 && key < node->left->key)
-        return rightRotate(node);
-    if (balance < -1 && key > node->right->key)
-        return leftRotate(node);
-    if (balance > 1 && key > node->left->key) {
-        node->left = leftRotate(node->left);
-        return rightRotate(node);
-    }
-    if (balance < -1 && key < node->right->key) {
-        node->right = rightRotate(node->right);
-        return leftRotate(node);
-    }
     return node;
 }
 
@@ -86,8 +47,8 @@ void preOrder(Node* root) {
 
 int main() {
     Node* root = nullptr;
-    int arr[] = {32, 51, 27};
-    for (int i = 0; i < 3; i++) {
+    int arr[] = {32, 51, 27, 83};
+    for (int i = 0; i < 4; i++) {
         root = insert(root, arr[i]);
     }
     preOrder(root);
