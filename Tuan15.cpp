d0fdd8b692834d1b474d05ad0fb6d2adaa10e178
namkhanh14185 <rbb6820@gmail.com>
2026-06-18 00:21:36 +0700

    fx

--- Tuan15.cpp
+++ Tuan15.cpp
@@ -2,56 +2,51 @@
 #include <vector>
 #include <queue>
 #include <string>
+#include <map>
 
 using namespace std;
 
-const int N = 11;
-const int INF = 1e9;
-const string city[N] = {
-    "Ha Noi", "Son Tay", "Hoa Binh", "Phu Ly", "Hung Yen",
-    "Hai Duong", "Hai Phong", "Uong Bi", "Bac Giang", "Bac Ninh", "Thai Nguyen"
-};
+class Graph {
+private:
+    map<string, vector<string>> adjList;
 
-struct Canh {
-    int v, weight;
-};
+public:
+    void themTuyenDuong(const string& tinhA, const string& tinhB) {
+        adjList[tinhA].push_back(tinhB);
+        adjList[tinhB].push_back(tinhA);
+    }
+    void duyetBảnDo(const string& diemBatDau) {
+        map<string, bool> visited;
+        queue<string> q;
 
-void them_Canh(vector<Canh> adj[], int u, int v, int w) {
-    adj[u].push_back({v, w});
-    adj[v].push_back({u, w});
-}
+        visited[diemBatDau] = true;
+        q.push(diemBatDau);
 
-void Dijkstra(const vector<Canh> adj[], int start, int dest) {
-    vector<int> dist(N, INF);
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        cout << "Bat dau hanh trinh tu: " << diemBatDau << " ---\n";
+        while (!q.empty()) {
+            string u = q.front();
+            q.pop();
 
-    dist[start] = 0;
-    pq.push({0, start});
+            cout << " > Da ghe tham: " << u << "\n";
 
-    while (!pq.empty()) {
-        int d = pq.top().first;
-        int u = pq.top().second;
-        pq.pop();
-
-        if (d > dist[u]) continue;
-
-        for (auto edge : adj[u]) {
-            if (dist[u] + edge.weight < dist[edge.v]) {
-                dist[edge.v] = dist[u] + edge.weight;
-                pq.push({dist[edge.v], edge.v});
+            for (const string& v : adjList[u]) {
+                if (!visited[v]) {
+                    visited[v] = true;
+                    q.push(v);
+                }
             }
         }
     }
-    cout << "Khoang cach ngan nhat tu " << city[start] << " den " << city[dest] << " la: " << dist[dest] << " km\n";
-}
+};
 
 int main() {
-    vector<Canh> adj[N];
-    them_Canh(adj, 0, 1, 42);  
-    them_Canh(adj, 0, 5, 58);  
-    them_Canh(adj, 5, 6, 45); 
-    them_Canh(adj, 6, 7, 40); 
+    Graph banDoMienBac;
 
-    Dijkstra(adj, 0, 7); 
+    banDoMienBac.themTuyenDuong("Ha Noi", "Bac Ninh");
+    banDoMienBac.themTuyenDuong("Ha Noi", "Thai Nguyen");
+    banDoMienBac.themTuyenDuong("Bac Ninh", "Bac Giang");
+    banDoMienBac.themTuyenDuong("Bac Giang", "Uong Bi");
+    banDoMienBac.duyetBảnDo("Ha Noi");
+
     return 0;
 }
