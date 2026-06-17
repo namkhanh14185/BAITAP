11aba08cb7561d89cd1d213c7f54409dc1ffe257
namkhanh14185 <rbb6820@gmail.com>
2026-06-18 00:22:36 +0700

    fx

--- Tuan15.cpp
+++ Tuan15.cpp
@@ -6,47 +6,43 @@
 
 using namespace std;
 
-class Graph {
-private:
-    map<string, vector<string>> adjList;
+map<string, vector<string>> adj; // Định nghĩa đồ thị bằng Tên -> Danh sách tên kề
 
-public:
-    void themTuyenDuong(const string& tinhA, const string& tinhB) {
-        adjList[tinhA].push_back(tinhB);
-        adjList[tinhB].push_back(tinhA);
-    }
-    void duyetBảnDo(const string& diemBatDau) {
-        map<string, bool> visited;
-        queue<string> q;
+void them_Canh(string u, string v) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
 
-        visited[diemBatDau] = true;
-        q.push(diemBatDau);
+void BFS_TheoTen(string start) {
+    map<string, bool> visited;
+    queue<string> q;
 
-        cout << "Bat dau hanh trinh tu: " << diemBatDau << " ---\n";
-        while (!q.empty()) {
-            string u = q.front();
-            q.pop();
+    visited[start] = true;
+    q.push(start);
 
-            cout << " > Da ghe tham: " << u << "\n";
+    while (!q.empty()) {
+        string u = q.front();
+        q.pop();
 
-            for (const string& v : adjList[u]) {
-                if (!visited[v]) {
-                    visited[v] = true;
-                    q.push(v);
-                }
+        cout << u << "\n";
+
+        for (string v : adj[u]) {
+            if (!visited[v]) {
+                visited[v] = true;
+                q.push(v);
             }
         }
     }
-};
+}
 
 int main() {
-    Graph banDoMienBac;
+    them_Canh("Ha Noi", "Son Tay");
+    them_Canh("Ha Noi", "Hoa Binh");
+    them_Canh("Ha Noi", "Hai Duong");
+    them_Canh("Hai Duong", "Hai Phong");
+    them_Canh("Hai Phong", "Uong Bi");
 
-    banDoMienBac.themTuyenDuong("Ha Noi", "Bac Ninh");
-    banDoMienBac.themTuyenDuong("Ha Noi", "Thai Nguyen");
-    banDoMienBac.themTuyenDuong("Bac Ninh", "Bac Giang");
-    banDoMienBac.themTuyenDuong("Bac Giang", "Uong Bi");
-    banDoMienBac.duyetBảnDo("Ha Noi");
-
+    cout << "Duyet BFS tim kiem theo ten tinh thanh:\n";
+    BFS_TheoTen("Ha Noi");
     return 0;
 }
