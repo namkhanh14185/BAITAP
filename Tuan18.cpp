f20df55c6ce5e023bba4d3622627b0c75116bfe2
namkhanh14185 <rbb6820@gmail.com>
2026-06-30 00:42:37 +0700

    fx

--- Tuan18.cpp
+++ Tuan18.cpp
@@ -1,57 +1,34 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <map>
 #include <algorithm>
-#include <iomanip>
-
 using namespace std;
-#define VO_CUNG 1e9
 
-struct Canh { int u, v, w; bool operator<(const Canh& o) const { return w < o.w; } };
+struct Canh { int u, v, w; };
+string ten[30]; int sn = 0; vector<Canh> ds;
 
-map<string, int> chuyen_ten;
-map<int, string> chuyen_so;
-int so_nut = 0;
-
-int lay_id(string s) {
-    if (chuyen_ten.find(s) == chuyen_ten.end()) {
-        chuyen_ten[s] = so_nut; chuyen_so[so_nut] = s; so_nut++;
-    }
-    return chuyen_ten[s];
+int id(string s) {
+    for(int i=0; i<sn; i++) if(ten[i]==s) return i;
+    ten[sn] = s; return sn++;
 }
-
-int tim_goc(int i, vector<int>& cha) { return cha[i] == -1 ? i : cha[i] = tim_goc(cha[i], cha); }
+int tim(int i, int cha[]) { return cha[i] == -1 ? i : cha[i] = tim(cha[i], cha); }
 
 int main() {
     system("chcp 65001 > nul");
-    vector<Canh> ds;
-    auto them = [&](string u, string v, int w) { ds.push_back({lay_id(u), lay_id(v), w}); };
-    them("Hà nội", "Hải dương", 55); them("Hải dương", "Hải phòng", 45);
-    them("Hà nội", "Phủ lý", 60); them("Hà nội", "Hoà Bình", 75);
+    auto add = [](string u, string v, int w){ ds.push_back({id(u), id(v), w}); };
+    add("Hà nội", "Hải dương", 55); add("Hải dương", "Hải phòng", 45);
+    add("Hà nội", "Phủ lý", 60); add("Hà nội", "Hoà Bình", 75);
 
-    sort(ds.begin(), ds.end());
-    vector<int> cha(so_nut, -1);
-    vector<vector<int>> mt(so_nut, vector<int>(so_nut, VO_CUNG));
-    int tong = 0, dem = 0;
+    sort(ds.begin(), ds.end(), [](Canh a, Canh b){ return a.w < b.w; });
+    int cha[30], tong = 0; fill(cha, cha+sn, -1);
 
-    for (auto& e : ds) {
-        int g1 = tim_goc(e.u, cha), g2 = tim_goc(e.v, cha);
-        if (g1 != g2) {
-            mt[e.u][e.v] = e.w; mt[e.v][e.u] = e.w;
-            tong += e.w; dem++; cha[g1] = g2;
+    for(auto& e : ds) {
+        int g1 = tim(e.u, cha), g2 = tim(e.v, cha);
+        if(g1 != g2) {
+            cout << ten[e.u] << "-" << ten[e.v] << ":" << e.w << "\n";
+            tong += e.w; cha[g1] = g2;
         }
     }
-    cout << "\n" << tong << "\n            ";
-    for (int i = 0; i < so_nut; i++) cout << setw(12) << chuyen_so[i];
-    cout << "\n";
-    for (int i = 0; i < so_nut; i++) {
-        cout << setw(12) << chuyen_so[i];
-        for (int j = 0; j < so_nut; j++) {
-            if (mt[i][j] == VO_CUNG) cout << setw(12) << "-";
-            else cout << setw(12) << mt[i][j];
-        }
-        cout << "\n";
-    }
+    cout << tong << "\n";
     return 0;
 }
